fb80b00fe34552e5033e6f80271cb3946c79f7ee
Peter Zhang <peter@0g.ai>
2024-03-21 19:08:23 +0800

    add cli

--- inabox/create-s3-bucket.sh
+++ inabox/create-s3-bucket.sh
@@ -2,7 +2,7 @@
 set -e
 
 S3_BUCKET="test-zgda-blobstore"
-S3_REGION="us-east-2"
+S3_REGION="us-east-1"
 
 if AWS_ACCESS_KEY_ID=localstack AWS_SECRET_ACCESS_KEY=localstack \
     aws s3api head-bucket --endpoint-url=$AWS_URL --bucket "$S3_BUCKET" 2>/dev/null; then
--- inabox/deploy/localstack.go
+++ inabox/deploy/localstack.go
@@ -114,7 +114,7 @@ func DeployResources(pool *dockertest.Pool, localStackPort, metadataTableName, b
 	}
 
 	cfg := aws.ClientConfig{
-		Region:          "us-east-2",
+		Region:          "us-east-1",
 		AccessKey:       "localstack",
 		SecretAccessKey: "localstack",
 		EndpointURL:     fmt.Sprintf("http://0.0.0.0:%s", localStackPort),
